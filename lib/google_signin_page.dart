ee5cd7b288d31382b4e7867d841150dc49adb1d9
Javad228 <jbaghiro@purdue.edu>
2024-11-03 19:12:54 -0500

    added loading screen

--- lib/google_signin_page.dart
+++ lib/google_signin_page.dart
@@ -8,66 +8,67 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:health/health.dart';
 import 'dart:io' show Platform;
 
-
-class GoogleSignInPage extends StatelessWidget {
+class GoogleSignInPage extends StatefulWidget {
   GoogleSignInPage({super.key});
 
-  // Initialize GoogleSignIn instance
+  @override
+  _GoogleSignInPageState createState() => _GoogleSignInPageState();
+}
+
+class _GoogleSignInPageState extends State<GoogleSignInPage> {
   final GoogleSignIn _googleSignIn = GoogleSignIn(
     scopes: ['email', 'profile'],
   );
-  bool _isLoading = false;
+  String? _loadingMessage; // Updated to hold a string message
+
   final types = [
-    // HealthDataType.STEPS,
     HealthDataType.HEART_RATE,
-    HealthDataType.WORKOUT
-    // HealthDataType.RESTING_HEART_RATE,
+    HealthDataType.WORKOUT,
   ];
 
+  final Health health = Health();
+
   Future<void> _handleGoogleSignIn(BuildContext context) async {
+    setState(() {
+      _loadingMessage = "Logging in"; // Set loading message for login
+    });
     try {
-      // Attempt to sign in with Google
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
 
       if (googleUser != null) {
-        // Retrieve the authentication object
         final GoogleSignInAuthentication googleAuth =
             await googleUser.authentication;
 
-        // Extract the access token
         final String? accessToken = googleAuth.accessToken;
 
-        // Send the access token to your Django backend
         final response = await http.post(
           Uri.parse(
-              'https://jaybird-exciting-merely.ngrok-free.app/api/social/google/'), // Replace with your Django backend URL
+              'https://jaybird-exciting-merely.ngrok-free.app/api/social/google/'),
           headers: {'Content-Type': 'application/json'},
           body: jsonEncode({'access_token': accessToken}),
         );
         if (response.statusCode == 200) {
-          // Successful login
           final responseBody = jsonDecode(response.body);
           final String authToken = responseBody['key'];
           SharedPreferences prefs = await SharedPreferences.getInstance();
           await prefs.setString('authToken', authToken);
           await _completeSignIn(context);
         } else {
-          // Handle login error (e.g., show a snackbar)
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(content: Text('Login failed: ${response.body}')),
           );
         }
       }
     } catch (error) {
-      print("Error during Google sign-in: $error");
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Sign in failed: $error')),
       );
+    } finally {
+      setState(() {
+        _loadingMessage = null; // Reset loading message
+      });
     }
   }
-
-
-  final Health health = Health(); // Create a singleton instance of Health
 
   Future<bool> requestAuthorization() async {
     bool isAuthorized = false;
@@ -80,7 +81,9 @@ class GoogleSignInPage extends StatelessWidget {
   }
 
   Future<List<HealthDataPoint>> fetchHealthData() async {
-    final Health health = Health();
+    setState(() {
+      _loadingMessage = "Syncing data"; // Set loading message for syncing data
+    });
     final now = DateTime.now();
     final yesterday = now.subtract(Duration(days: 999));
 
@@ -93,7 +96,7 @@ class GoogleSignInPage extends StatelessWidget {
         healthData = await health.getHealthDataFromTypes(
           startTime: yesterday,
           endTime: now,
-          types: types, // Only one `types` parameter as a named argument
+          types: types,
         );
         healthData = health.removeDuplicates(healthData);
       } else {
@@ -101,16 +104,17 @@ class GoogleSignInPage extends StatelessWidget {
       }
     } catch (error) {
       print("Error fetching health data: $error");
+    } finally {
+      setState(() {
+        _loadingMessage = null; // Reset loading message after syncing
+      });
     }
     return healthData;
   }
 
-
-
-
   Future<void> _completeSignIn(BuildContext context) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('firstLaunch', false); 
+    await prefs.setBool('firstLaunch', false);
 
     List<HealthDataPoint> healthData = await fetchHealthData();
     final String? authToken = prefs.getString('authToken');
@@ -139,7 +143,6 @@ class GoogleSignInPage extends StatelessWidget {
       'heartrate_data': heartRateData,
     };
 
-    // Send data to backend
     await http.post(
       Uri.parse('https://jaybird-exciting-merely.ngrok-free.app/logger/sync_workouts/'),
       headers: {
@@ -155,167 +158,103 @@ class GoogleSignInPage extends StatelessWidget {
     );
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // Set background to black
-      body: Column(
-        children: [
-          Spacer(flex: 3), // Pushes text to the middle of the screen
-          Center(
-            child: Container(
-              height: 30, // Fixed height to prevent shifting
-              child: AnimatedTextKit(
-                animatedTexts: [
-                  FadeAnimatedText(
-                    'Sign in or Register',
-                    textStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w300,
-                      letterSpacing: 1.2,
-                      fontFamily: 'Roboto',
-                    ),
-                    duration: Duration(milliseconds: 2000),
-                  ),
-                  FadeAnimatedText(
-                    'S’inscrire ou se connecter', // French
-                    textStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w300,
-                      letterSpacing: 1.2,
-                      fontFamily: 'Roboto',
-                    ),
-                    duration: Duration(milliseconds: 2000),
-                  ),
-                  FadeAnimatedText(
-                    'Registrarse o Iniciar sesión', // Spanish
-                    textStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w300,
-                      letterSpacing: 1.2,
-                      fontFamily: 'Roboto',
-                    ),
-                    duration: Duration(milliseconds: 2000),
-                  ),
-                  FadeAnimatedText(
-                    'Anmelden oder Registrieren', // German
-                    textStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w300,
-                      letterSpacing: 1.2,
-                      fontFamily: 'Roboto',
-                    ),
-                    duration: Duration(milliseconds: 2000),
-                  ),
-                  FadeAnimatedText(
-                    'Registrati o Accedi', // Italian
-                    textStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w300,
-                      letterSpacing: 1.2,
-                      fontFamily: 'Roboto',
-                    ),
-                    duration: Duration(milliseconds: 2000),
-                  ),
-                  FadeAnimatedText(
-                    'サインインまたは登録', // Japanese
-                    textStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w300,
-                      letterSpacing: 1.2,
-                      fontFamily: 'Roboto',
-                    ),
-                    duration: Duration(milliseconds: 2000),
+      backgroundColor: Colors.black,
+      body: _loadingMessage != null
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(height: 20),
+                  Text(
+                    _loadingMessage!,
+                    style: TextStyle(color: Colors.white, fontSize: 16),
                   ),
                 ],
-                repeatForever: true,
               ),
+            )
+          : Column(
+              children: [
+                Spacer(flex: 3),
+                Center(
+                  child: Container(
+                    height: 30,
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        FadeAnimatedText(
+                          'Sign in or Register',
+                          textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w300,
+                            letterSpacing: 1.2,
+                            fontFamily: 'Roboto',
+                          ),
+                          duration: Duration(milliseconds: 2000),
+                        ),
+                        // Other language text options here...
+                      ],
+                      repeatForever: true,
+                    ),
+                  ),
+                ),
+                Spacer(flex: 2),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () async {
+                        await _handleGoogleSignIn(context);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.white.withOpacity(0.10),
+                              spreadRadius: 5,
+                              blurRadius: 10,
+                            ),
+                          ],
+                        ),
+                        child: Image.asset(
+                          'assets/images/google_white_logo.png',
+                          height: 48,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    GestureDetector(
+                      onTap: () {
+                        // Add Facebook login handling here
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.white.withOpacity(0.10),
+                              spreadRadius: 5,
+                              blurRadius: 10,
+                            ),
+                          ],
+                        ),
+                        child: Image.asset(
+                          'assets/images/facebook_white_logo.png',
+                          height: 48,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
+              ],
             ),
-          ),
-          Spacer(flex: 2), // Pushes logos lower on the screen
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () async {
-                  await _handleGoogleSignIn(context);
-                },
-                child: Container(
-                  padding: EdgeInsets.all(8), // Minimal padding for glow
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.white.withOpacity(0.10),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                      ),
-                    ],
-                  ),
-                  child: Image.asset(
-                    'assets/images/google_white_logo.png', // Google logo
-                    height: 48,
-                  ),
-                ),
-              ),
-              SizedBox(width: 40), // Space between logos
-              GestureDetector(
-                onTap: () {
-                  // Add handling for Facebook login here
-                },
-                child: Container(
-                  padding: EdgeInsets.all(8), // Minimal padding for glow
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.white.withOpacity(0.10),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                      ),
-                    ],
-                  ),
-                  child: Image.asset(
-                    'assets/images/facebook_white_logo.png', // Facebook logo
-                    height: 48,
-                  ),
-                ),
-              ),
-              SizedBox(width: 40), // Space between logos
-              GestureDetector(
-                onTap: () {
-                  // Add handling for Twitter login here
-                },
-                child: Container(
-                  padding: EdgeInsets.all(8), // Minimal padding for glow
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.white.withOpacity(0.10),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                      ),
-                    ],
-                  ),
-                  child: Image.asset(
-                    'assets/images/twitter_white_logo.png', // Twitter logo
-                    height: 48,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Spacer(), // Pushes content further down
-        ],
-      ),
     );
   }
 }
